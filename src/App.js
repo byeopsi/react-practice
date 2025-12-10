7e97eedc2a18241acb4f4a2b74b8c96576c638a8
byeopsi <4217609@gmail.com>
2025-12-10 14:07:27 +0900

    lifecycle

--- src/App.js
+++ src/App.js
@@ -8,8 +8,23 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
     return (
       <div>
@@ -22,7 +37,11 @@ class App extends Component {
         <button onClick={() => this.scrollBox.scrollToBottom()}>
           맨 밑으로
         </button> */}
-        <IterationSample />
+        {/* <IterationSample /> */}
+        <ErrorBoundary>
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
